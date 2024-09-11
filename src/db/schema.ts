f0daa2aa3bab07ffc0ad08893c4b37ce3c621e1c
jenny.santos <jennyfer.conceicao@atoscapital.com.br>
2024-09-10 22:02:31 -0300

    :feat Criação das rotas e functions

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,21 @@
 import { pgTable, text, integer, timestamp } from 'drizzle-orm/pg-core';
+import { createId } from '@paralleldrive/cuid2';
 
+//Observar que entre 'goals' é o nome q ira estar no banco de dado, por isso é interessante não fazer kamelcase, já o nome da const pode ser normal pois será utilizado dentro do js
 export const goals = pgTable('goals', {
-    id: text('id').primaryKey(),
+    id: text('id').primaryKey().$defaultFn(() => createId()),//Essa função ocorre do lado do js, nesse caso não necessica de migration
     title: text('title').notNull(),
     desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
+    createdAt: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+})
+
+export const goalCompletions = pgTable('goal_completions', {
+    id: text('id').primaryKey().$defaultFn(() => createId()),
+    goalId: text('goal_id')
+    .references(() => goals.id)//a references está referenciando como o nome já diz uma forenkey, ou seja chave estrangeira da Table acima a goals
+    .notNull(),
     createdAt: timestamp('created_at', { withTimezone: true })
     .notNull()
     .defaultNow(),
---
+++ src/functions/get-week-pending-goals.ts
@@ -0,0 +1,57 @@
+import dayjs from "dayjs";
+import { db } from "../db";
+import { goalCompletions, goals } from "../db/schema";
+import { and, sql, lte, count, gte, eq } from 'drizzle-orm';
+
+export async function getWeekPendingGoals(){
+    const firstDayOfWeek = dayjs().startOf('week').toDate()
+    const lastDayOfWeek = dayjs().endOf('week').toDate()
+
+    //É um exemplo de uma (COLUMN TABLE EXPRESSION) DO SQL
+    const goalsCreatedUpToWeek = db.$with('goals_created_up_to_week').as(
+        /*
+            No Drizzle ORM, a cláusula lte (abreviação de less than or equal to) 
+            é usada para realizar consultas em que você deseja comparar se o valor de 
+            um campo é menor ou igual a um determinado valor.
+        */
+        db.select({
+            id: goals.id,
+            title: goals.title,
+            desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+            createdAt: goals.createdAt,
+        })
+        .from(goals)
+        .where(lte(goals.createdAt, lastDayOfWeek))
+    )
+
+    const goalCompletioCounts = db.$with('goal_completion_counts').as(
+        db.select({
+            goalId: goalCompletions.goalId,
+            completionCount: count(goalCompletions.id).as('completionCount'),
+        })
+        .from(goalCompletions)
+        .where(
+            and(
+                gte(goalCompletions.createdAt, firstDayOfWeek),
+                lte(goalCompletions.createdAt, lastDayOfWeek)
+            )
+        )
+        .groupBy(goalCompletions.goalId)
+    )
+
+    //Observar que quando não adiciono o $ no with, siginifca que ele não será uma COLUMN TABLE EXPRESSION
+    const pedingGoals = await db
+    .with(goalsCreatedUpToWeek, goalCompletioCounts)
+    .select({
+        id: goalsCreatedUpToWeek.id,
+        title: goalsCreatedUpToWeek.title,
+        desiredWeeklyFrequency: goalsCreatedUpToWeek.desiredWeeklyFrequency,
+        completionCount: sql`
+            COALESCE(${goalCompletioCounts.completionCount}, 0)
+        `.mapWith(Number),
+    })
+    .from(goalsCreatedUpToWeek)
+    .leftJoin(goalCompletioCounts, eq(goalCompletioCounts.goalId, goalsCreatedUpToWeek.id))
+
+    return { pedingGoals }
+}
